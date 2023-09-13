5f3bc607cb0b4bada4a13778ea0a4bd22d85e32e
Deepesh Sharma <deep92112018@gmail.com>
2023-09-13 12:35:39 +0530

    Learn Page - Task  4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from "./components/nav/Nav";
 import Courses from "./pages/app/courses/Courses";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Details from "./pages/app/details/Details";
+import Learn from "./pages/app/learn/Learn";
 
 function App() {
   const router = createBrowserRouter([
@@ -17,12 +18,14 @@ function App() {
           path: '/courses',
           children: [
             {path: '', element: <Courses />},
-            {path: ':courseId', element: <Details/>}
+            { path: ':courseId', element: <Details/>}
           ]
-
+        },
+        {
+          path: '/learn/:courseId', element: <Learn/>
         }
       ],
-
+      
     },
   ]);
   
